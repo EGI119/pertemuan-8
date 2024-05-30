a19b8fecd89084c9e9a1b346a9867d2c2f8d9278
EGI119 <egidianadipradana@students.amikom.ac.id>
2024-05-30 14:53:32 +0700

    first commit

---
+++ vue-project-praktikum-8/src/components/Artikel.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>hallo masbro</h1>
+    <p>ini adalah artikel masbro</p>
+    <img src="./src/assets/images.jpg" alt="" width="100px" >
+  </div>
+</template>
